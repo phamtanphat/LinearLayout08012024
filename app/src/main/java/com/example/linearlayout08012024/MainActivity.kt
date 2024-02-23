cf56d77ffb623b4a0ded038f2a5fcb34c9f53cdb
phamtanphat <phatdroid94@gmail.com>
2024-02-23 18:58:30 +0700

    Initial commit

---
+++ app/src/main/java/com/example/linearlayout08012024/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.linearlayout08012024
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import java.util.Objects
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
